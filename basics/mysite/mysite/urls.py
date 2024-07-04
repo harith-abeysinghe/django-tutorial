efcb337d6dff504f69cf7bad79f7041bff9dd410
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-07-04 17:42:04 +0530

    Part 2 initial

--- basics/mysite/mysite/urls.py
+++ basics/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("polls.urls")),
     path("polls/", include("polls.urls")), # The include() function allows referencing other URLconfs.
     path('admin/', admin.site.urls),
 ]
--- basics/mysite/mysite/wsgi.py
+++ basics/mysite/mysite/wsgi.py
@@ -1,6 +1,6 @@
 """
 WSGI config for mysite project.
-
+WSGI - Web Server Gateway Interface
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
